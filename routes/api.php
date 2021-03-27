3ca1f61ed920deca7f5fa9ce1c80f2c1a336cf03
hideyukisugamata <hideyuki.sugamata@gmai.com>
2021-03-27 17:24:34 +0900

    fix2

--- routes/api.php
+++ routes/api.php
@@ -21,12 +21,12 @@ use App\Http\Controllers\CommentsController;
 |
 */
 
-Route::apiResource('/shares',SharesController::class);
-Route::post('/register',[RegisterController::class,'post']);
-Route::post('/login',[LoginController::class,'post']);
-Route::post('/logout',[LogoutController::class,'post']);
-Route::get('/user',[UsersController::class,'get']);
-Route::put('/user',[UsersController::class,'put']);
-Route::post('/like',[LikesController::class,'post']);
-Route::delete('/like',[LikesController::class,'delete']);
-Route::post('/comment',[CommentsController::class,'post']);
+Route::apiResource('/shares', SharesController::class);
+Route::post('/register', [RegisterController::class, 'post']);
+Route::post('/login', [LoginController::class, 'post']);
+Route::post('/logout', [LogoutController::class, 'post']);
+Route::get('/user', [UsersController::class, 'get']);
+Route::put('/user', [UsersController::class, 'put']);
+Route::post('/like', [LikesController::class, 'post']);
+Route::delete('/like', [LikesController::class, 'delete']);
+Route::post('/comment', [CommentsController::class, 'post']);
